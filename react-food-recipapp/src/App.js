e3b1743f8a1b94298031f5d1627497375c129787
Abe-Tiz1 <106752541+Abe-Tiz@users.noreply.github.com>
2023-04-02 14:43:11 +0300

    improveskill

--- react-food-recipapp/src/App.js
+++ react-food-recipapp/src/App.js
@@ -1,9 +1,19 @@
+import HeroSection from "./components/HeroSection";
+import ImproveSkill from "./components/ImproveSkill";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container main" >
+        
+        <HeroSection />
+        <ImproveSkill />
+
+      </div>
+
+
     </div>
   );
 }
--- react-food-recipapp/src/components/ImproveSkill.jsx
+++ react-food-recipapp/src/components/ImproveSkill.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 const ImproveSkill = () => {
+    const list =[
+        "Learn new recipies",
+        "Experment with food",
+        "Write your own recipies",
+        "Know nutrition facts",
+        "Get cooking tips",
+        "Get ranked"
+    ]
   return (
-    <div>
-      hello
+    <div className='section improve-skill'>
+        <div className="col img">
+          <img src="img/gallery/img_10.jpg" alt="" />
+        </div>
+        <div className="col typography">
+            <h1 className='title'>Improve Your Culinary Skills</h1>
+              {list.map((item,index) =>(
+                    <p className='skill-item' key={index}>{item}</p>
+                ))}
+            <button className="btn">Signup Now</button>
+        </div>
+    
+    
     </div>
   )
 }
